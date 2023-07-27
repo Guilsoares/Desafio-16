2a154a206e51fc4ea910db04ff13d24cac469602
Guilherme Soares <g.l.soares@outlook.com>
2023-07-27 12:59:53 -0300

    desafio concluído

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemap = require('gulp-sourcemaps');
 const imagemin = require('gulp-imagemin');
-const uglify = require('gulp-uglify')
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate')
 
 function compilaSass(){
     return gulp.src('./source/main.scss')
@@ -20,8 +21,16 @@ function imageminificada(){
     .pipe(gulp.dest("./build/images"))
 }
 
+function compilaJS(){
+    return gulp.src('./source/script/*.js')
+    .pipe(uglify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest('./build/script'))
+}
+
 exports.default = function(){
     gulp.watch("./source/*.scss", {ignoreInitial:false}, gulp.series(compilaSass))
     gulp.watch("./source/images/*", {ignoreInitial:false}, gulp.series(imageminificada))
+    gulp.watch("./source/script/*", {ignoreInitial:false}, gulp.series(compilaJS))
 
 }
---
+++ source/script/script.js
@@ -0,0 +1,5 @@
+window.alert('Bem vindo! Index para testes dos desafio proposto')
+
+function sum(a,b){
+    return a + b
+}
